2e9dc424f513d9b8d1e0d626c9dc96cfc6f2c325
elifsirin42 <elifsirin42@gmail.com>
2021-05-30 17:29:32 +0300

    if-else-if dosyası istenenlere göre oluşturulmuştur.

---
+++ Pratikler/if-else-if/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time>=6 && time<11)
+            Console.WriteLine("Günaydınn!");
+            else if(time<=18)
+            Console.WriteLine("İyi Günlerr!");
+            else
+                Console.WriteLine("İyi Gecelerr!");
+                
+            string sonuc = time <= 10 ? "İyi Günler!" : "İyi Geceler!";
+
+            sonuc = time >= 6 && time <11 ? "Günaydinn!" : time <= 18 ? "İyi günlerr!" : "İyi Gecelerr!";
+
+            Console.WriteLine(sonuc);       
+        }
+    }
+}
